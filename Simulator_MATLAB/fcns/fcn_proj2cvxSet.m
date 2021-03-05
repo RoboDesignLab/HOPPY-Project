32353506c4395232945173cfca2a519c91610dd2
YanranDing <david.ding92@gmail.com>
2021-03-05 15:46:55 -0600

    edit optimization options

--- Simulator_MATLAB/fcns/fcn_proj2cvxSet.m
+++ Simulator_MATLAB/fcns/fcn_proj2cvxSet.m
@@ -4,6 +4,7 @@ function x = fcn_proj2cvxSet(u,A,b)
     Aeq = [];
     beq = [];
     
-    options = optimoptions('quadprog','Display','off');
+    %options = optimoptions('quadprog','Display','off');
+    options = [];
     x = quadprog(H,f,A,b,Aeq,beq,[],[],[],options);
 end
